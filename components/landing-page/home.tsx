1ec60d320d087fd8f0dbd3bcf55315a9a3414f6b
Fiiwe Barry <fiiwebarry@yahoo.com>
2026-05-14 09:14:57 +0100

    fixes

--- components/landing-page/home.tsx
+++ components/landing-page/home.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <section className="grid grid-justify-items-center py-20 text-center">
-      <h1 className=" font-bold text-[#3fbcaa]">
+    <section className="grid  py-20 lg:px-20">
+      <h1 className=" text-xl font-bold text-[#3fbcaa]">
         Permanent Voter's Card Action Movement
       </h1>
-      <h2 className="text-lg text-gray-300">
+      <h2 className="text-6xl font-bold w-8/12 mt-2 text-gray-300">
         Empowering Voters, Strengthening Democracy
       </h2>
-      <h5 className="text-md text-gray-400">
+      <h5 className="text-md text-gray-400 mt-2">
         Join the Movement for Electoral Reform
       </h5>
-      <p className="text-md text-gray-400 md:w-1/2 mx-auto mt-3">
+      <p className="text-md text-gray-400 md:w-1/2 mt-3">
         Millions of eligible Nigerian voters remain unregistered or have
         uncollected PVCs due to lack of awareness, difficulty locating INEC
         centres, and fragmented grassroots mobilisation efforts. No single
