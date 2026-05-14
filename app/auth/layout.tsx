bd22d5a596560de288443a701563086ff7e5bc42
Izzy <izima@resilience17.com>
2026-05-14 09:07:56 +0100

    feat: added fonts and gradient for auth pages

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -7,17 +7,12 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col md:flex-row items-stretch h-[calc(100vh+280px)] sm:h-[calc(100vh+200px)] md:h-screen relative bg-[#10200e] z-10">
-      <Image
-        src={bg}
-        loading="eager"
-        alt="Background"
-        className="-mt-2 -z-10 h-full md:w-1/2 xl:w-[45%] w-full"
-      />
-      {/* </div> */}
-
+    <div
+      className="flex justify-center items-center w-full md:h-screen bg-linear-to-br hero-bg"
+    >
+      {/* <div className="absolute inset-0 hero-grid" /> */}
       <div
-        className="md:right-[8%] xl:right-[12%] w-11/12 sm:w-4/5 md:w-2/3 max-w-4xl p-6 sm:p-8 lg:p-12 absolute not-md:right-1/2 not-md:translate-x-1/2 top-1/2 -translate-y-1/2 bg-white rounded-lg flex flex-col"
+        className="w-11/12 sm:w-4/5 md:w-2/3 max-w-4xl p-6 sm:p-8 lg:p-12 bg-white rounded-lg flex flex-col"
         style={{ boxShadow: "1px 5px 14px 0px #0000001A" }}
       >
         <h1 className="text-4xl font-extrabold text-[#10200e]/80 text-center">
