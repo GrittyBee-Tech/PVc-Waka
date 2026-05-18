c569a404f57bd8897713d17dcb04c1e6b2766ced
Fiiwe Barry <fiiwebarry@yahoo.com>
2026-05-18 18:09:06 +0100

    fixes

--- components/dashboard/VolunteerDashboard.tsx
+++ components/dashboard/VolunteerDashboard.tsx
@@ -1,8 +1,11 @@
 export default function VolunteerDashboard() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Volunteer Dashboard</h1>
-            <p>Welcome to your volunteer dashboard. Here you can track your registered users and register new users.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Volunteer Dashboard</h1>
+      <p>
+        Welcome to your volunteer dashboard. Here you can track your registered
+        users and register new users.
+      </p>
+    </div>
+  );
 }
