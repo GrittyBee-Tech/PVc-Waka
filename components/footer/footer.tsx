16ee01f431663db9661074ef3fb87366f5a208d7
Fiiwe Barry <fiiwebarry@yahoo.com>
2026-05-21 22:12:41 +0100

    fixes

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { IoFootstepsSharp } from "react-icons/io5";
+import Logo from "../ui/Logo";
 
 const Footer = () => {
   return (
-    <footer className="grid md:grid-flow-col lg:px-20 items-center justify-between p-6 shadow-2xl">
-      <div className=" grid grid-flow-col gap-2 text-2xl text-black  font-bold">
-        <h1 className="text-2xl">PVC WAKA</h1>
-        <IoFootstepsSharp className=" text-4xl" />
-      </div>
+    <footer className="grid md:grid-flow-col  bg-[#F9FDFA] border-border border-t lg:px-20 items-center justify-between py-5 shadow-xl">
+      <Logo />
 
       <div className="grid md:grid-flow-col items-center  gap-10 text-lg">
         <p className="text-black font-dm-sans text-sm">
           © 2026 PVC Waka All Right Reserved-{" "}
           <a
-            className="text-black font-bold"
+            className="text-primary font-bold"
             href="https://www.abenolfoundation.org/"
             target="_blank"
             rel="noopener noreferrer"
