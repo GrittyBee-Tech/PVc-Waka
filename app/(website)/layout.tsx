70ff93b64db3805a3a71aab4c0032420b29810fb
Izima01 <kingsleyizima@gmail.com>
2026-05-21 08:53:15 +0100

    fix: changed layout and colors on landing page

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,39 +1,3 @@
-import type { Metadata } from "next";
-import {
-  Montserrat,
-  Playfair_Display,
-  DM_Sans,
-  Space_Grotesk,
-} from "next/font/google";
-import "../globals.css";
-
-const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  display: "swap", // Prevents layout shift
-  variable: "--font-playfair-display", // Useful for Tailwind
-});
-const dmSans = DM_Sans({
-  subsets: ["latin"],
-  display: "swap", // Prevents layout shift
-  variable: "--font-dm-sans", // Useful for Tailwind
-});
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  display: "swap", // Prevents layout shift
-  variable: "--font-space-grotesk  ", // Useful for Tailwind
-});
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  display: "swap", // Prevents layout shift
-  variable: "--font-montserrat",
-});
-
-export const metadata: Metadata = {
-  title: "PVC WAKA - Empowering Voters, Strengthening Democracy",
-  description:
-    "PVC WAKA is a platform dedicated to empowering Nigerian voters by providing easy access to information about Permanent Voter's Card (PVC) registration and collection. Our mission is to strengthen democracy by ensuring that every eligible voter can easily check their PVC status, find INEC centers, and track their registration progress online. Join us in our movement for electoral reform and make your voice heard.",
-};
-
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 
@@ -43,14 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen border-b bg-linear-to-b from-accent/40 to-background ">
-      <div className=""></div>
-      <div className=""></div>
-      <div className="">
-        <Navbar />
-        {children}
-        <Footer />
-      </div>
+    <div className="min-h-screen border-b bg-linear-to-b from-accent/40 to-background">
+      <Navbar />
+      {children}
+      <Footer />
     </div>
   );
 }
--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -2,9 +2,9 @@ import { IoFootstepsSharp } from "react-icons/io5";
 
 const Logo = () => {
   return (
-    <a href="/" className="grid grid-flow-col gap-2 text-[#0A140F] font-bold">
-      <h1 className="text-2xl md:text-2xl">PVC WAKA</h1>
-      <IoFootstepsSharp className="text-2xl md:text-3xl" />
+    <a href="/" className="grid grid-flow-col gap-2 text-primary text-pretty font-bold">
+      <h1 className="text-xl sm:text-2xl md:text-2xl">PVC WAKA</h1>
+      <IoFootstepsSharp className="text-xl sm:text-2xl md:text-3xl" />
     </a>
   );
 };
