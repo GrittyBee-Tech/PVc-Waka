9ec9be624a94959cbb87408c3e700b710d52f912
Izima01 <kingsleyizima@gmail.com>
2026-05-26 12:06:17 +0100

    fix: added location origin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,8 +3,9 @@ import { auth } from "./auth";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || window.location.origin,
   plugins: [inferAdditionalFields<typeof auth>()],
+
 });
 
 export const { signUp, signIn, signOut, useSession } = authClient;
