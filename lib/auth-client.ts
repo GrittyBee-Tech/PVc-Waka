1411c0a76c379f71dad11a49712745576d9c5f9d
Izima01 <kingsleyizima@gmail.com>
2026-05-26 12:11:07 +0100

    fix: added next_public

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,9 +3,8 @@ import { auth } from "./auth";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || window.location.origin,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [inferAdditionalFields<typeof auth>()],
-
 });
 
 export const { signUp, signIn, signOut, useSession } = authClient;
