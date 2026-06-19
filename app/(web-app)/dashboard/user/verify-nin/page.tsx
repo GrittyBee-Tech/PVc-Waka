df26fbd170142d25917897bbe0435f81674ca89b
Fiiwe Barry <fiiwebarry@yahoo.com>
2026-06-19 17:42:51 +0100

    chores(paystack): changing route

--- app/(web-app)/dashboard/user/verify-nin/page.tsx
+++ app/(web-app)/dashboard/user/verify-nin/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { IoFootstepsSharp } from "react-icons/io5";
 import { useAuth } from "@/hooks/useAuth";
 import { showToast } from "@/utils/constants/toast";
+import Modal from "@/components/ui/modal";
 
 export default function PayStack() {
   const [loading, setLoading] = useState(true);
@@ -16,7 +17,6 @@ export default function PayStack() {
   const isInitialized = useRef(false);
 
   // 2. Prevent execution if the flag has already flipped to true
-
   useEffect(() => {
     if (isInitialized.current) return;
     isInitialized.current = true;
@@ -68,18 +68,49 @@ export default function PayStack() {
   }, []);
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
+    <section>
       {loading && (
-        <div className="grid mx-auto">
-          <div className="grid grid-flow-col gap-2 text-white w-max text-pretty font-bold">
-            <h1 className="text-xl sm:text-2xl md:text-7xl">PVC WAKA</h1>
-            <IoFootstepsSharp className="text-xl sm:text-2xl md:text-3xl" />
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
+          <div className="grid mx-auto">
+            <div className="grid grid-flow-col gap-2 text-white w-max text-pretty font-bold">
+              <h1 className="text-xl sm:text-2xl md:text-7xl">PVC WAKA</h1>
+              <IoFootstepsSharp className="text-xl sm:text-2xl md:text-3xl" />
+            </div>
+            <p className="text-xl text-center animate-ping text-white mt-6">
+              Opening payment...
+            </p>
           </div>
-          <p className="text-xl text-center animate-ping text-white mt-6">
-            Opening payment...
-          </p>
         </div>
       )}
-    </div>
+      <Modal
+        isOpen={true}
+        position="absolute"
+        title="Verify Your Information"
+        closeButton={false}
+        actions={
+          <>
+            <button className="md:px-6 md:py-2 py-2  px-4 md:text-lg font-bold rounded bg-primary border border-green-700 text-white disabled:opacity-60">
+              Verify nin
+            </button>
+          </>
+        }
+      >
+        <div className="space-y-4">
+          <p className="font-bold">You are Almost there {user?.firstName}</p>
+          <p className="text-primary">
+            To complete your profile setup kindly verify your NIN
+          </p>
+          <div className="rounded-lg border border-yellow-400/30 bg-yellow-50 p-4 text-sm text-yellow-900">
+            <p className="font-semibold">Verification Fee has been Paid</p>
+            <p> Payment was Successful</p>
+          </div>
+          <p className="text-primary font-dm-sans -mt-3">
+            Kindly confirm your NIN.
+          </p>
+
+          <p className="text-xs font-dm-sans text-muted-foreground"></p>
+        </div>
+      </Modal>
+    </section>
   );
 }
