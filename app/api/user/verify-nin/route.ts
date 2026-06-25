95ee085f513f5c3de50ae4261337ba461277c7bb
Fiiwe Barry <fiiwebarry@yahoo.com>
2026-06-25 18:04:48 +0100

    chore(lumid): redirecting route to dashboard on successful

--- app/api/user/verify-nin/route.ts
+++ app/api/user/verify-nin/route.ts
@@ -16,29 +16,34 @@ export const POST = withDb(async (request: Request) => {
       );
     }
 
-    // const data = await verifyNIN(nin);
-    // const apiKey = process.env.LUMIID_SECRET_KEY;
-    // const response = await fetch("https://api.lumiid.com/v1/ng/nin-basic/", {
-    //   method: "POST",
-    //   headers: {
-    //     Authorization: `Bearer ${apiKey}`,
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({ nin }),
-    // });
-    // const data = await response.json();
-    // console.log(data);
+     const data = await verifyNIN(nin);
+    console.log(data);
 
-    // if (!data.success) {
-    //   return Response.json(
-    //     {
-    //       success: false,
-    //       message: data.message,
-    //       code: data.code,
-    //     },
-    //     { status: 400 },
-    //   );
-    // }
+    if (!data.success) {
+      return Response.json(
+        {
+          success: false,
+          message: data.message,
+          code: data.code,
+        },
+        { status: 400 },
+      );
+    }
+    if(!data.summary.verified){
+
+       await UserModel.updateOne(
+      { _id: session?.user.id },
+      { ninStatus: "rejected" },
+    );
+    return Response.json(
+      {
+        success: false,
+        summary: { verified: false, verification_type: "NIN" },
+        data: { success: false, message: "Verification unsuccessful" },
+      },
+      { status: 200 },
+    );
+    }
 
     await UserModel.updateOne(
       { _id: session?.user.id },
