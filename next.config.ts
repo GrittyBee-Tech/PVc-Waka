a4805c5563f80a84a077200899b18c5d47b16296
Izima01 <kingsleyizima@gmail.com>
2026-06-28 23:09:46 +0100

    fix: updated next config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,22 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fallback for node modules when running on the client (browser)
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        "fs/promises": false,
+        net: false,
+        tls: false,
+        crypto: false,
+        dns: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
