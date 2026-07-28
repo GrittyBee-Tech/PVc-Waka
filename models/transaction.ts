c7723b391138d3a007e41459ab85db6abc6f2473
Izima01 <kingsleyizima@gmail.com>
2026-07-28 22:46:32 +0100

    feat: added flutterwave instead of paystack

--- models/transaction.ts
+++ models/transaction.ts
@@ -3,19 +3,30 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 export interface ITransaction extends Document {
   user_id: string;
   reference: string;
+  provider: "paystack" | "flutterwave";
+  provider_reference?: string; // Optional field for provider's reference
   purpose: string;
-  access_code: string;
+  access_code?: string;
   amount: number;
   status: "pending" | "success" | "failed";
   createdAt: Date;
   updatedAt: Date;
+  meta?: Record<string, any>; // Optional field for additional metadata
 }
 
 const TransactionSchema: Schema<ITransaction> = new Schema(
   {
     user_id: { type: String, required: true },
     reference: { type: String, required: true },
-    access_code: { type: String, required: true },
+    provider: {
+      type: String,
+      enum: ["paystack", "flutterwave"],
+      required: true,
+      default: "flutterwave",
+    },
+    provider_reference: { type: String }, // Optional field for provider's reference
+    meta: { type: Schema.Types.Mixed, default: {} },
+    access_code: { type: String },
     purpose: { type: String, required: true },
     amount: { type: Number, required: true },
     status: {
